dc43c1a77f11db6137bb9400744ccae7618c7dea
duckythescientist <seantaylormurphy@gmail.com>
2018-09-22 23:36:59 -0400

    Add compile flag to nop trace and debug
    This is really important for performance of tight loops

--- log.h
+++ log.h
@@ -21,8 +21,13 @@ typedef void (*log_LockFn)(void *udata, int lock);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
+#if defined(NODEBUG) || defined(RELEASE)
+#define log_trace(...) 
+#define log_debug(...) 
+#else
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#endif
 #define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
